3defc7dcb273dfa7447321b13c1a37ca48718f86
tbd-shafa <shafa@technobd.com>
2025-01-22 17:27:21 +0600

    login and show profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -75,17 +75,6 @@ export default function Profile() {
     return <p>Loading...</p>;
   }
 
-//   return (
-//     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-//       <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-md">
-//         <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">Profile</h1>
-//         <p><strong>Name:</strong> {user.firstName} {user.lastName}</p>
-//         <p><strong>Email:</strong> {user.email}</p>
-//         <p><strong>Gender:</strong> {user.gender}</p>
-//       </div>
-//     </div>
-//   );
-// }
 
 return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
